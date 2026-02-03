308242c653cd70cdf3b57496febcec3d762f0deb
phuchuynhtan4103131 <s4103131@rmit.edu.vn>
2026-02-03 15:30:53 +0700

    changing from clinic to phone,laptop repairing

--- Main.java
+++ Main.java
@@ -5,16 +5,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         Console console = System.console();
-        System.out.println("========-> Welcome to the system! <-==============" );
+        System.out.println("========-> Welcome to Sunshine Repairshop! <-==============");
         System.out.println("First, you need to identify yourself, please type 1 for register or 0 to Login");
         int user = scnr.nextInt();
-        if(console != null){
-            if(user == 1){
-                login();
-            }
-            else if (user == 0) {
-                register();
-            }
+        // if(console != null){
+        //     if(user == 1){
+        //         login();
+        //     }
+        //     else if (user == 0) {
+        //         register();
+        //     }
+        // }
+        menu();
+        int selection = scnr.nextInt();
+        switch (selection){
+            case 1:
+                screenReplacement();
+                break;
+            case 2:
+                battery();
         }
         
         scnr.close();
@@ -22,7 +31,7 @@ public class Main {
         public static void register(){
             
             System.out.println("==========================================================");
-            System.out.println("||            NEOLEAGUE SYSTEM INFRASTRUCTURE           ||");
+            System.out.println("||                  SUNSHINE REPAIR HUB                 ||");
             System.out.println("||             CREATE A NEW ACCOUNT TO BEGIN            ||");
             System.out.println("==========================================================");
             Scanner scanner = new Scanner(System.in);
@@ -46,8 +55,8 @@ public class Main {
         public static void login(){
             Console console = System.console();
             System.out.println("==========================================================");
-                System.out.println("||            NEOLEAGUE SYSTEM INFRASTRUCTURE           ||");
-                System.out.println("||                ENTER YOUR USERNAME AND PASSWORD TO LOGIN               ||");
+                System.out.println("||            NEOLEAGUE SYSTEM INFRASTRUCTURE               ||");
+                System.out.println("||            ENTER YOUR USERNAME AND PASSWORD TO LOGIN     ||");
                 System.out.println("==========================================================");
                 String username = console.readLine();
                 System.out.println("Saving....");
@@ -55,5 +64,59 @@ public class Main {
                 char[] password = console.readPassword("Enter your password: ");
                 System.out.printf("USER: %s | PASSWORD: %s \n",username, password );
         }
+        public static void menu(){
+            int speed = 10;
+            slowPrint("Welcome user!", speed);
+            slowPrint("==========================================", speed);
+            slowPrint("       HEALTH SERVICE CLINIC MENU         ", speed);
+            slowPrint("||     Your Device, Our Priority.        ||", speed);
+            slowPrint("==========================================", speed);
+            
+            slowPrint("\n--- REPAIR SERVICES ---", speed);
+            slowPrint("1.  Smartphone Screen Replacement", speed);
+            slowPrint("2.  Battery Health & Replacement", speed);
+            slowPrint("3.  Laptop Keyboard/Trackpad Repair", speed);
+            slowPrint("4.  Water Damage Recovery", speed);
+            slowPrint("5.  OS Reinstallation & Software Troubleshooting", speed);
+            slowPrint("6.  Data Recovery & Backup Services", speed);
+            slowPrint("7.  Hardware Upgrades (RAM/SSD)", speed);
+            slowPrint("8.  Charging Port Cleaning & Repair", speed);
+            slowPrint("\n==========================================================", speed);
+            slowPrint("   Enter the service number to get an estimate.   ", speed);
+        }
+        public static void screenReplacement(){
+            Scanner scnr = new Scanner(System.in);
+            int speed = 10; 
+            slowPrint("You have selected Smartphone Screen Replacement", speed);
+            slowPrint("First, please select your phone", speed);
+            String[] phones = {"1. iPhone 17 Pro Max", "2. iPhone 17", "3. Samsung Galaxy S25 Ultra",
+                "4. Samsung Galaxy A56 5G", "5. Google Pixel 10 Pro XL", "6. Google Pixel 9a",
+                "7. OnePlus 15", "8. Xiaomi Redmi Note 15 Pro", "9. Nothing Phone (3a) Pro",
+                "10. Motorola Moto G86 Power", "11. Fairphone 5", "12. Samsung Galaxy Z Fold 7",
+                "13. iPhone 16e", "14. iPhone Air", "15. Samsung Galaxy S25 FE"};
+                for(String phone :phones){
+                    slowPrint(phone, speed);
+                }
+        }
+        public static void battery(){
+            Scanner scnr = new Scanner(System.in);
+            int delay = 10;
+            slowPrint("You have chosen battery health check service", delay);
+            slowPrint("This service may requires technical reviews and face to face replacement, We suggest you may make a reservation for this", delay);
+            slowPrint("", delay);
+        }
+
+        public static void slowPrint(String text, int delay){
+            for(char c : text.toCharArray()){
+                System.out.print(c);
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+             System.out.println();
+        }
+
     
 }
